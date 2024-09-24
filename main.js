62c294843a051404dac9bbba2ccd80e8b0590cb9
Dania <dmluna@scl.edu.gt>
2024-09-24 09:16:38 -0600

    rama dl-proyectos hecha

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 import { navBarVid } from "./components/navBar/navBar.js";
+import { proyectos } from "./components/project/project.js";
 let DOM = document.querySelector("#root");
 
 /* navBAr*/
 let navBAr = document.createElement('section');
 navBAr.className= "nav-Bar";
-navBAr.innerHTML="hola mundo soy navBAr";
 navBAr.appendChild(navBarVid);
 DOM.appendChild(navBAr);
 
 
 let project = document.createElement('section');
 project.classList= "project";
-project.innerText="hola mundo soy project";
+project.appendChild(proyectos());
 DOM.appendChild(project);
 
 
