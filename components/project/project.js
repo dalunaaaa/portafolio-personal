66f83f5530b66a92131cf48ee84d283ecbd24c77
Dania <dmluna@scl.edu.gt>
2024-09-26 10:03:49 -0600

    Segunda version del portafolio -se agrega el navbar -se agrega el header -se agrega un elemento item de guia

--- components/project/project.js
+++ components/project/project.js
@@ -1,24 +1,14 @@
+import { header } from "../header/header.js";
+
+
+
 function proyectos(){
     let section = document.createElement('section');
     section.appendChild(header());
     section.appendChild(item());
     return section
 }
-function header (){
-    let div = document.createElement('div');
-    div.className ='header';
 
-    let h2 = document.createElement('h2');
-    h2.innerHTML = "proyectos"
-    div.appendChild (h2);
-
-    let btn = document.createElement('btn');
-    btn.innerHTML = "GitHub";
-    div.appendChild(btn);
-
-    return div;
-
-}
 function item (){
     let div = document.createElement('div');
     div.innerText= "item de lista";
